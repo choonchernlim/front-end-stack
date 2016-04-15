7a34a0d5dfd0ef370c794e2f9059e4243beda1ce
Choon-Chern Lim <limc@mayo.edu>
2016-04-15 13:37:13 -0500

    prod env

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,9 @@ module.exports = Object.assign({}, baseConfig, {
     // 1. Only assets/ dir will contain hashed filenames
     // 2. When used in typical JEE app, dist dir will also contain WEB-INF/ and we don't want this
     //    plugin to wipe that dir off
-    new CleanPlugin(path.join(baseConfig.output.path, '/assets')),
+    new CleanPlugin(['assets'], {
+      root: baseConfig.output.path
+    }),
 
     // Minify JS without source map and suppress any warnings.
     new webpack.optimize.UglifyJsPlugin({
@@ -19,8 +21,19 @@ module.exports = Object.assign({}, baseConfig, {
       }
     }),
 
+    // To prevent the following warnings in browser console:-
+    // "It looks like you're using a minified copy of the development build of React.
+    // When deploying React apps to production, make sure to use the production build
+    // which skips development warnings and is faster."
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+
     // Prevents the inclusion of duplicate code into bundle and instead applies a copy
     // of the function at runtime, which results smaller file size
     new webpack.optimize.DedupePlugin()
   )
-});
+})
+;
