906998c0e3e211497c65318f45ee26e0da216425
Choon-Chern Lim <limc@mayo.edu>
2016-05-19 14:45:26 -0500

    Cleaner solution for `"Invalid regular expression: /^\api\(.*)\/?$/: Unmatched ')'"` with running `npm start` in Windows.

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,5 +1,6 @@
 const baseConfig = require('./webpack.base.config');
 const path = require('path');
+const url = require('url');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const packageJson = require('./package.json');
@@ -43,12 +44,8 @@ module.exports = Object.assign({}, baseConfig.webpackOptions, {
 
     // Server side proxy when `<context_root>/api/*` is called
     proxy: {
-      // `path.join(..) is used to remove possible trailing slash from the context root.
-      // However, `path` also handles platform-specific file path and turns forward slash to
-      // backslash in Windows. Since this is a proxy URI, any backslash (from Windows)
-      // need to be converted to forward slash to prevent this error:
-      // "Invalid regular expression: /^\api\(.*)\/?$/: Unmatched ')'"
-      [path.join(packageJson.config.context_root, '/api/*').replace(/\\/g, '/')]: {
+      // Using `url.resolve(..)` to handle possible trailing slash in context root
+      [url.resolve(packageJson.config.context_root, '/api/*')]: {
         target: 'https://localhost:8443',
         secure: false
       }
