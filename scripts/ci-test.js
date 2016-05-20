997f9f39dd2af50f7e92f6ba45a6d3ed37850733
Mike <meteoric@rocketmail.com>
2016-05-20 11:40:30 -0500

    extra slash

--- scripts/ci-test.js
+++ scripts/ci-test.js
@@ -4,9 +4,11 @@
  * Cross-platform script to execute Mocha tests and generate result file for CI.
  */
 const process = require('process');
+const path = require('path');
 const exec = require('./exec');
 const testDirPath = process.env.npm_package_config_test_dir_path;
 const mochaOpts = process.env.npm_package_config_mocha_opts;
 const reportDirPath = process.env.npm_package_config_report_dir_path;
+const mochaFilePath = path.join(reportDirPath, 'test-results.xml');
 
-exec(`mocha ${testDirPath} ${mochaOpts} --reporter mocha-junit-reporter --reporter-options mochaFile=${reportDirPath}/test-results.xml`);
+exec(`mocha ${testDirPath} ${mochaOpts} --reporter mocha-junit-reporter --reporter-options mochaFile=${mochaFilePath}`);
