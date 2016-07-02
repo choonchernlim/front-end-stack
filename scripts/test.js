54a9d03f33e4b834e2fc387de50cefbe5266e05e
Mike <meteoric@rocketmail.com>
2016-07-02 07:50:21 -0500

    `npm test ./test/abc` and `npm run test:watch ./test/abc` to run only tests within ./test/abc.

--- scripts/test.js
+++ scripts/test.js
@@ -1,11 +1,19 @@
 /**
- * `npm test`
+ * `npm test` - to run all tests.
+ * `npm test ./test/submodule` - to run all tests within `./test/submodule`.
  *
  * Cross-platform script to execute Mocha tests and lint the test files.
  */
 const process = require('process');
 const script = require('./script');
-const testDirPath = process.env.npm_package_config_test_dir_path;
 const mochaOpts = script.mochaOpts;
+
+// remove first 2 arguments... first arg is usually the path to nodejs, and
+// the second arg is script location
+const args = process.argv.slice(2);
+
+// if user passes argument, set first argument as `testDirPath`, otherwise use default value from
+// package.json
+const testDirPath = args.length ? args[0] : process.env.npm_package_config_test_dir_path;
 
 script.run(`mocha ${testDirPath} ${mochaOpts} --colors && eslint ${testDirPath} --color`);
