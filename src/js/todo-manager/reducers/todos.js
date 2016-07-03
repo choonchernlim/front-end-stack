e5cc651ff4c3bd113592e78ca02aceea75bf0630
Choon-Chern Lim <meteoric@rocketmail.com>
2016-07-02 21:53:32 -0500

    todo manager action constant

--- src/js/todo-manager/reducers/todos.js
+++ src/js/todo-manager/reducers/todos.js
@@ -1,15 +1,16 @@
 import { List, fromJS } from 'immutable';
+import { ADD_TODO, TOGGLE_TODO } from '../actions';
 
 const todo = (state, action) => {
   switch (action.type) {
-    case 'ADD_TODO':
+    case ADD_TODO:
       return fromJS({
         id: action.id,
         text: action.text,
         completed: false
       });
 
-    case 'TOGGLE_TODO':
+    case TOGGLE_TODO:
       if (state.get('id') !== action.id) {
         return state;
       }
@@ -23,10 +24,10 @@ const todo = (state, action) => {
 
 const todos = (state = new List(), action) => {
   switch (action.type) {
-    case 'ADD_TODO':
+    case ADD_TODO:
       return state.push(todo(undefined, action));
 
-    case 'TOGGLE_TODO':
+    case TOGGLE_TODO:
       return state.map((t) => todo(t, action));
 
     default:
--- src/js/todo-manager/reducers/visibility-filter.js
+++ src/js/todo-manager/reducers/visibility-filter.js
@@ -1,6 +1,8 @@
+import { SET_VISIBILITY_FILTER } from '../actions';
+
 const visibilityFilter = (state = 'SHOW_ALL', action) => {
   switch (action.type) {
-    case 'SET_VISIBILITY_FILTER':
+    case SET_VISIBILITY_FILTER:
       return action.filter;
 
     default:
