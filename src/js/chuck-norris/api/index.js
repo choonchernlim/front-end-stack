9742e4c814567e48244a7a5e3e39b98fcb213a8c
Choon-Chern Lim <meteoric@rocketmail.com>
2016-07-04 15:32:13 -0500

    specs for chuck norris api

--- src/js/chuck-norris/api/index.js
+++ src/js/chuck-norris/api/index.js
@@ -1,7 +1,10 @@
 import fetch from 'isomorphic-fetch';
 
+export const RANDOM_JOKE_SERVER = 'http://api.icndb.com';
+export const RANDOM_JOKE_URI = '/jokes/random';
+
 export function getRandomJokeApi() {
-  return fetch('http://api.icndb.com/jokes/random')
+  return fetch(RANDOM_JOKE_SERVER + RANDOM_JOKE_URI)
     .then(response => {
       if (response.status >= 400) {
         throw new Error(`Unable to get a random joke: ${response.statusText}`);
