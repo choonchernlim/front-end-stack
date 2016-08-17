45e4080028185befe953950f89780fe760a3508c
Choon-Chern Lim <limc@mayo.edu>
2016-08-17 14:21:32 -0500

    refactored app/

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, Route, IndexRoute, useRouterHistory } from 'react-router';
+import { Router, useRouterHistory } from 'react-router';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 import { syncHistoryWithStore } from 'react-router-redux';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import { Layout, Home, TodoManager, ChuckNorris, LookAndFeel } from './component';
-import store from './store';
+import configureStore from './store';
+import getRoutes from './routes';
 import packageJson from '../../../package.json';
 import '../../scss/index.scss';
 
@@ -19,19 +19,17 @@ const browserHistory = useRouterHistory(createBrowserHistory)({
   basename: packageJson.config.context_root
 });
 
+// configure store
+const store = configureStore(browserHistory);
+
+const routes = getRoutes(store);
+
 // Create an enhanced history that syncs navigation events with the store
 const history = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history}>
-      <Route path="/" component={Layout}>
-        <IndexRoute component={Home} />
-        <Route path="look-and-feel" component={LookAndFeel} />
-        <Route path="chuck-norris" component={ChuckNorris} />
-        <Route path="todo-manager" component={TodoManager} />
-      </Route>
-    </Router>
+    <Router history={history} routes={routes} />
   </Provider>,
   document.getElementById('app')
 );
