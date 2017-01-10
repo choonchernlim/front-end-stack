bf5815f8ef086207bea163e1f0dc5c3a792fc64a
Choon-Chern Lim <limc@mayo.edu>
2017-01-10 17:02:17 -0600

    flow typed create-reducer

--- src/js/common/utils/create-reducer.js
+++ src/js/common/utils/create-reducer.js
@@ -1,12 +1,22 @@
+// @flow
+import { List, Map, Record } from 'immutable';
+
+// ensure reducer state can be either Immutable List, Immutable Map or Immutable Record.
+type State = List<*> | Map<*, *> | Record<*>;
+
 /**
  * Creating reducer based on Redux recipe site in effort to create better flow-typed reducer.
+ *
  * See http://redux.js.org/docs/recipes/ReducingBoilerplate.html
  * See https://robwise.github.io/blog/using-flow-annotations-in-your-redux-reducers
  *
- * @param initialState
- * @param handlers
+ * @param initialState  Initial state
+ * @param handlers      Action handler
  */
-const createReducer = (initialState, handlers) => (state = initialState, action) => (
+const createReducer = (initialState: State, handlers: {}) => (
+  state: State = initialState,
+  action: { type: string }
+): State => (
   Object.prototype.hasOwnProperty.call(handlers, action.type) ?
     handlers[action.type](state, action) :
     state
