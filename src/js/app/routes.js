c8ec680f72f5186cf178b5f037afad71a66f6a20
Choon-Chern Lim <limc@mayo.edu>
2017-09-01 09:34:45 -0500

    page not found

--- src/js/app/routes.js
+++ src/js/app/routes.js
@@ -1,17 +1,21 @@
 // @flow
 import type { Element } from 'react';
 import React from 'react';
-import { IndexRoute, Route } from 'react-router';
+import { IndexRoute, Route, Redirect } from 'react-router';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import TodoManager from '../todo-manager/components/TodoManager';
 import ChuckNorris from '../chuck-norris/components/ChuckNorris';
+import NotFoundError from '../error/components/NotFoundError';
 
 const getRoutes = (): Element<*> => (
   <Route path="/" component={Layout}>
     <IndexRoute component={Home} />
     <Route path="chuck-norris" component={ChuckNorris} />
     <Route path="todo-manager" component={TodoManager} />
+    <Route path="error/page-not-found" component={NotFoundError} />
+
+    <Redirect from="*" to="error/page-not-found" />
   </Route>
 );
 
