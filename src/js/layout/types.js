4f3d822273560b57ed6c03e15411c528d79ed248
Choon-Chern Lim <limc@mayo.edu>
2018-01-24 15:59:30 -0600

    better action type names

--- src/js/layout/types.js
+++ src/js/layout/types.js
@@ -3,8 +3,8 @@
 type ActionTypes = { [key: string]: string };
 
 const ACTION_TYPES: ActionTypes = {
-  MENU_LEFT_OPENED: 'LAYOUT/MENU_LEFT_OPENED',
-  TOGGLE_MENU: 'LAYOUT/TOGGLE_MENU',
+  MENU_LEFT_OPENED: 'LAYOUT/MENU-LEFT-OPENED',
+  TOGGLE_MENU: 'LAYOUT/TOGGLE-MENU',
 };
 
 type ActionType = $Keys<typeof ACTION_TYPES>;
